5ea0cb01c9771ec9875c228328cb3828d02a8bf0
paul <paul.vdh@hotmail.fr>
2019-09-18 23:26:52 +0200

    suppression de installJavaProprietaire et ajout de installEclipse

--- run.sh
+++ run.sh
@@ -26,14 +26,16 @@ if [ $reponse = "y" ] || [ $reponse = "Y" ] ; then
 	installFlatpakUbuntu 2>> stderr.log
 	flatpakSetup 2>> stderr.log
 
-	installStandardApps 2>> stderr.log
-	installAppsDev 2>> stderr.log
-	installSteam 2>> stderr.log
-	installLutris 2>> stderr.log
+	#installStandardApps 2>> stderr.log
+	#installAppsDev 2>> stderr.log
+	#installSteam 2>> stderr.log
+	#installLutris 2>> stderr.log
 	installMinecraft 2>> stderr.log
 	#installWineForLutris 2>> stderr.log
+	installOpenJre 2>> stderr.log
+	installEclipse 2>> stderr.log
 	changeSwapSettings 2>> stderr.log
 	changeGnomeSettings 2>> stderr.log
-	removeSnap 2>>stderr.log
+	#removeSnap 2>>stderr.log
 	updateAndClean 2>> stderr.log
 fi

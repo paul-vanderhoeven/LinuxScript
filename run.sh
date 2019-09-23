74d85bf319bc88b53156a32eb9de09c241c7ddb8
paul <paul.vdh@hotmail.fr>
2019-09-23 21:52:39 +0200

    ajout de installEclipse

--- run.sh
+++ run.sh
@@ -7,16 +7,15 @@
 # Si vous avez une version d'Ubuntu différente ou une autre distribution que Ubuntu, certaines partie du script ne fonctionneront pas.
 # --------------------------------------------------------------------------------
 
-STDERR="stderr-$(date +%Y-%m-%d-%H:%M:%S).log"
-STDOUT="stdout-$(date +%Y-%m-%d-%H:%M:%S).log"
+STDERR="$(pwd)/stderr-$(date +%Y-%m-%d-%H:%M:%S).log"
+STDOUT="$(pwd)/stdout-$(date +%Y-%m-%d-%H:%M:%S).log"
+
 export TTY="$(tty)"
 
 if [ "$(id -u)" != 0 ] ; then
 	echo "Vous devez avoir les droits d'administrateurs !"
 	exit
 fi
-
-touch $STDOUT
 
 echo "Script de configuration et d'installation des logiciels principaux pour Paul VDH."
 echo "Ce script va installer les logiciels : Flatpak, VLC, LibreOffice, Git, Eclipse, Discord, Qbittorrent, Klavaro, Intellij, Dconf, Sublime Text, Gnome Tweaks, Steam, Lutris, Minecraft ..."
@@ -32,17 +31,18 @@ if [ $reponse = "y" ] || [ $reponse = "Y" ] ; then
 	installFlatpakUbuntu 2>> $STDERR >> $STDOUT
 	flatpakSetup 2>> $STDERR >> $STDOUT
 
-	#installStandardApps 2>> $STDERR 
-	#installAppsDev 2>> $STDERR
-	#installSteam 2>> $STDERR
-	#installLutris 2>> $STDERR
-	#installMinecraft 2>> $STDERR
-	#installWineForLutris 2>> $STDERR
-	#installOpenJre 2>> $STDERR
-	#installEclipse 2>> $STDERR
-	#changeSwapSettings 2>> $STDERR
-	#changeGnomeSettings 2>> $STDERR
-	#removeSnap 2>>$STDERR
-	#installLAMP 2>> $STDERR
+	#installStandardApps 2>> $STDERR >> $STDOUT 
+	#installAppsDev 2>> $STDERR >> $STDOUT
+	#installSteam 2>> $STDERR >> $STDOUT
+	#installLutris 2>> $STDERR >> $STDOUT
+	#installMinecraft 2>> $STDERR >> $STDOUT
+	#installWineForLutris 2>> $STDERR >> $STDOUT
+	#installOpenJre 2>> $STDERR >> $STDOUT
+	#installEclipse 2>> $STDERR >> $STDOUT
+	#changeSwapSettings 2>> $STDERR >> $STDOUT
+	#changeGnomeSettings 2>> $STDERR >> $STDOUT
+	installEclipse 2>> $STDERR >> $STDOUT
+	#removeSnap 2>>$STDERR >> $STDOUT
+	#installLAMP 2>> $STDERR >> $STDOUT
 	updateAndClean 2>> $STDERR >> $STDOUT
 fi
